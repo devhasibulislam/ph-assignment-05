285628e00e6e849a706cc9fe69390e4e0ffd4816
hasibulislam999 <hasib143sl@gmail.com>
2022-02-17 10:05:25 +0600

    ui almost done

--- js/script.js
+++ js/script.js
@@ -5,11 +5,9 @@ document.getElementById('first-portion').addEventListener('mouseover', function(
 document.querySelector('#site-image img').addEventListener('mouseover', function(){
     document.querySelector('#site-image img').style.transition = '1s linear';
     document.querySelector('#site-image img').style.transform = 'scale(1.05)';
-    document.querySelector('#site-image img').style.filter = 'blur(0px)';
 });
 
 document.querySelector('#site-image img').addEventListener('mouseout', function(){
     document.querySelector('#site-image img').style.transition = '1s linear';
     document.querySelector('#site-image img').style.transform = 'scale(1)';
-    document.querySelector('#site-image img').style.filter = 'blur(3px)';
 });
